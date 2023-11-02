811b371cb737084792d71386af8f985db7787b73
Jorge Lira <jorgelira542502@gmail.com>
2023-11-02 15:55:36 -0300

    commit das telas e autorização

---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  padding: 16px 20px;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  width: 100%;
+  cursor: pointer;
+  background-color: #698F3F;
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+  max-width: 350px;
+
+  &:hover {
+    background-color: #9AC171;
+  }
+`;
---
+++ src/routes/index.js
@@ -0,0 +1,29 @@
+import { Fragment } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+import Home from "../pages/Leads";
+import Signin from "../pages/Logar";
+import Signup from "../pages/Registro";
+
+const Private = ({ Item }) => {
+  const { signed } = useAuth();
+
+  return signed > 0 ? <Item /> : <Signin />;
+};
+
+const RoutesApp = () => {
+  return (
+    <BrowserRouter>
+      <Fragment>
+        <Routes>
+          <Route exact path="/home" element={<Private Item={Home} />} />
+          <Route path="/" element={<Signin />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route path="*" element={<Signin />} />
+        </Routes>
+      </Fragment>
+    </BrowserRouter>
+  );
+};
+
+export default RoutesApp;
